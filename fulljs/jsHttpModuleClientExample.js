b8633ccebc06841927e95d5051707658d2803a9f
Jeff Sheeron <jcs@san.rr.com>
2020-05-24 06:39:38 -0700

    Sample file cleanup

--- fulljs/jsHttpModuleClientExample.js
+++
@@ -1,9 +0,0 @@
-import https from 'https';
-
-https.get('https://www.lynda.com', (res) => {
-  console.log('Response status code: ', res.statusCode);
-
-  res.on('data', (chunk) => {
-    console.log(chunk.toString());
-  });
-});
